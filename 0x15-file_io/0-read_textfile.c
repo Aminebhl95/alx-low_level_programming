511ecf0de07cd997d7e582ff9bd8f2bd6a1c9320
Aminebhl95 <aminebahloule@gmail.com>
2023-07-10 14:39:21 -0700

    Adding files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,12 +3,12 @@
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file
+ * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the fuction can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, O_RONDLY);
+	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
